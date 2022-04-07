fb1f8d8bc0d2ec17be6895df8c24d65a25a1dae3
Alvin C. Nataputra <70255413+apricitea@users.noreply.github.com>
2022-04-07 12:06:16 +0700

    ganti jd pake bahasa indonesia

--- try.R
+++ try.R
@@ -1,10 +1,33 @@
-ihsg <- read.csv("C:/Users/Haci/Desktop/Metode Peramalan Deret Waktu/Project-20220127/IHSG7days.csv", sep=";")
-ts.ihsg <- ts(ihsg$JKSE.Adjusted)
-ts.plot(ts.ihsg)
-View(ihsg)
+library(TTR)
+library(forecast)
+library(aTSA)
+library(TSA)
 library(imputeTS)
+
+ihsg <- read.csv("C:/Users/Haci/Desktop/Metode Peramalan Deret Waktu/Project-20220127/IHSG7days.csv", sep=";")
 ihsg.imputed <- na_interpolation(ihsg)
 ggplot_na_imputations(ihsg$JKSE.Adjusted, ihsg.imputed$JKSE.Adjusted)
+View(ihsg.imputed)
+
+data.ts <- ts(ihsg.imputed$JKSE.Adjusted)
+ts.plot(data.ts)
+adf.test(data.ts)
+
+seasonal.ts <- ts(ihsg.imputed$JKSE.Adjusted, frequency = 183)
+diff.seasonal.ts <- diff(seasonal.ts,1)
+acf(diff.seasonal.ts, lag.max=100)
+
+eacf(data.ts)
+eacf(ihsg.diff)
+#ARIMA(0,1,2)
+#ARIMA(0,1,3)
+#ARIMA(1,1,3)
+
+model1 <- Arima(data.ts, order=c(0,1,2), method="ML")
+model2 <- Arima(data.ts, order=c(0,1,2), include.drift = TRUE, method="ML")
+model3 <- Arima(data.ts, order=c(0,1,3), method="ML")
+model4 <- Arima(data.ts, order=c(1,1,3), method="ML")
+model1$aic; model2$aic; model3$aic; model4$aic;
 
 data.ts <- ts(ihsg.imputed$JKSE.Adjusted)
 ts.plot(data.ts, xlab="Time Period", ylab="IHSG Adjusted Closing Price", 
@@ -28,10 +51,7 @@ diff.bc.ihsg <- diff(data.ts, difference=1)
 acf(diff.bc.ihsg, lag.max = 50, main = "ACF IHSG Adjusted Closing Price") 
 pacf(diff.bc.ihsg, lag.max = 50, main = "PACF IHSG Adjusted Closing Price")
 
-library(forecast)
-library(aTSA)
 adf.test(ihsg.diff)
-
 ts.plot(diff.bc.ihsg)
 
 bcd <- boxcox((ihsg.diff+329) ~ 1)
@@ -63,3 +83,7 @@ diff.seasonal.ts <- diff(seasonal.ts,1)
 acf(diff.seasonal.ts, lag.max = 100)
 plot.ts(ds.ts)
 plot.ts(ihsg.diff)
+
+par(mfrow=c(1,2))
+ts.plot(data.ts, main="IHSG Adjusted Closing Price 1991-2020")
+ts.plot(data.ts[0:2000], main="IHSG Adjusted Closing Price 1991-1996")
